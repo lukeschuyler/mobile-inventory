d43889c9e230116e94463f92492f1e79e55502c3
Luke Schuyler <lukeschuyler1@gmail.com>
2017-05-03 11:51:47 -0500

    added qr scanner compoent to home for test

--- index.ios.js
+++ index.ios.js
@@ -1,52 +1,74 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
+'use strict';
+ 
 import React, { Component } from 'react';
+ 
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  NavigatorIOS,
+  TouchableOpacity,
+  TouchableHighlight,
+  Linking,
 } from 'react-native';
+ 
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 export default class mobile_inventory extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+  onSuccess(e) {
+    alert(e.data)
+    // Linking.openURL(e.data).catch(err => console.error('An error occured', err))
   }
+
+  render() {
+      return (
+        <NavigatorIOS
+          initialRoute={{
+            component: QRCodeScanner,
+            title: 'Scan Code',
+            passProps: {
+              onRead: this.onSuccess.bind(this),
+              topContent: <Text style={styles.centerText}>Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.</Text>,
+              bottomContent: <TouchableOpacity style={styles.buttonTouchable}><Text style={styles.buttonText}>OK. Got it!</Text></TouchableOpacity>
+            }
+          }}
+          style={{flex: 1}}
+        />
+      )
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  button: {
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'pink',
+    borderRadius: 3,
+    padding: 32,
+    width: 100,
+    marginTop: 64,
+    marginBottom: 64,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+ 
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+ 
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+ 
+  buttonTouchable: {
+    padding: 16,
   },
 });
 
